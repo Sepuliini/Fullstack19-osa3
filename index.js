05d400bda4038651061780288c58b1ae4349f4f9
Severi Nättilä <severi.nattila@gmail.com>
2019-07-20 13:15:29 +0300

    jatkoa osaan 3

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log('hei maailma!')
+const http = require('http')
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040009642",
+    id: 1,
+    
+  },
+  {
+    name: "Person personen",
+    number: "050699942",
+    id: 2,
+    
+  },
+  {
+    name: "Antti Mattila",
+    number: "040509139",
+    id: 3,
+  },
+  {
+    name: "Antti uusimattila",
+    number: "0405091666",
+    id: 4,
+  },
+  {
+    name: "Antti Kattila",
+    number: "0405091666",
+    id: 5,
+  }
+]
+
+
+
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(persons))
+})
+
+const port = 3001
+app.listen(port)
+console.log(`Server running on port ${port}`)
