d43637fcbce7b8ea23fa881e66ea37004f82070f
Severi Nättilä <severi.nattila@gmail.com>
2019-12-30 16:10:12 +0200

    tehtävät 3.1-3.6

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 
 let persons = [
   {
@@ -30,13 +33,98 @@ let persons = [
   }
 ]
 
+app.use(bodyParser.json())
 
+app.get('/info', (request, response) => {
+  const maara = persons.length
+  response.send(`
+    <h3>info<h3>
+    <p> Phonebook has info for ${maara} people <p>
+    <p>${new Date()}</p>
+  `)
+})
 
-const app = http.createServer((request, response) => {
+app.get('/api/persons', (request, response) => {
   response.writeHead(200, { 'Content-Type': 'application/json' })
   response.end(JSON.stringify(persons))
 })
 
+app.get('/api/persons/id', (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(persons))
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person == null) {
+    response.status(404).end()
+  } else {
+    response.json(person)
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  console.log("request:", request.body)
+  console.log("body:", body)
+  console.log("nimi:", body.name)
+  console.log("numero:", body.number)
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+  console.log("index of:",persons.filter(person => (person.name === body.name)))
+
+  if (persons.filter(person => (person.name === body.name)).length > 0) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
+  if (persons.filter(person => (person.number === body.number)).length > 0) {
+    return response.status(400).json({ 
+      error: 'number must be unique' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => p.id))
+    : 0
+  return maxId + 1
+}
+
 const port = 3001
 app.listen(port)
 console.log(`Server running on port ${port}`)
+console.log(persons)
+//console.log(app)
